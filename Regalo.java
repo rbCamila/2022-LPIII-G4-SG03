05d4b4e237d0f9261e1288772c6002a9558aa435
rbCamila <camila.rodriguezbrown@ucsm.edu.pe>
2022-08-21 18:19:17 -0500

    regalo en Rama2

---
+++ Regalo.java
@@ -0,0 +1,18 @@
+
+public class Regalo {
+	public static String elegir(int vida) {
+		String gift ="";
+		switch(vida) {
+		case 1: 
+			gift = "Un pasaje al caribe";
+			break; 
+		case 2:
+			gift = "Una visita al museo más cercano a tu casa";
+			break;
+		case 3: 
+			gift = "Una entrada al cine";
+			break;
+		}
+		return gift;
+	}
+}
