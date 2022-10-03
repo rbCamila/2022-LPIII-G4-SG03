7d9d27dc143e511aefec767fb0fe6698df5294e6
rbCamila <102678762+rbCamila@users.noreply.github.com>
2022-10-03 12:45:20 -0500

    Add files via upload

---
+++ EJERCICIO_1_POLIMORFISMO/Figura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EJERCICIO_1_POLIMORFISMO;
+
+/**
+ *
+ * @author ACER
+ */
+public abstract class Figura {
+    abstract public double ObtenerArea();
+}
